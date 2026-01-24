4558be13aefd1f0dc85e71c54fcaa761d34b8657
Deepnitha N <deepnitha03@gmail.com>
2026-01-24 14:08:57 +0530

    Add VariablesDemo program

---
+++ basics/VariablesDemo.java
@@ -0,0 +1,13 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 17;
+        double height = 168.5;
+        char grade = 'A';
+        String name = "Deepnitha";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+    }
+}
